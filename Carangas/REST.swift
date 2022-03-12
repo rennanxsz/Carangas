b0aabea58264821701a72d64265950183d3ff61f
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-03-11 22:42:09 -0300

    manipulating sessions
    Criando e configurando uma sessão s ser utilizada.

--- Carangas/REST.swift
+++ Carangas/REST.swift
@@ -10,4 +10,25 @@ import Foundation
 
 class REST {
     
+    private static let basePath = "https://carangas.herokuapp.com/cars"
+    
+    private static let configuration: URLSessionConfiguration = {
+        let config = URLSessionConfiguration.default
+        //Liberando ou negando que o usuario use o 4g ou 3g
+        config.allowsCellularAccess = false //true
+        //Informando o tipo de requisição, neste caso foi feito uma requisição de um Json
+        config.httpAdditionalHeaders = ["Content-Type": "application/json"]
+        //Configuranto o tempo que o usuario vai aguardar
+        config.timeoutIntervalForRequest = 30.0
+        //Configurando o maximo de requisições
+        config.httpMaximumConnectionsPerHost = 5
+        
+        return config
+    }()
+    
+    //Sessão criada manualmente.
+    private static let session = URLSession(configuration: configuration) //URLSession.shared
+    
+    
+    
 }
