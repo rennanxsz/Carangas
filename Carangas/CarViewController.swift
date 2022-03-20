cec5252dbb8f282745a6bdc8b511630f0185d5f5
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-03-19 22:09:34 -0300

    Complet APP
    App completo.

--- Carangas/CarViewController.swift
+++ Carangas/CarViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import WebKit
+import Foundation
 
 class CarViewController: UIViewController {
 
@@ -30,9 +31,22 @@ class CarViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        title = car.name
         lbBrand.text = car.brand
         lbGasType.text = car.gas
         lbPrice.text = "R$ \(car.price)"
+
+        let name = (title! + "+" + car.brand).replacingOccurrences(of: " ", with: "+")
+        let urlString = "https://www.google.com.br/search?q=\(name)&tbm=isch"
+        let url = URL(string: urlString)!
+        let request = URLRequest(url: url)
+        
+        webView.allowsBackForwardNavigationGestures = true
+        webView.allowsLinkPreview = true
+        webView.navigationDelegate = self
+        webView.uiDelegate = self
+        webView.load(request)
+        
     }
     
     
@@ -42,3 +56,17 @@ class CarViewController: UIViewController {
     }
 
 }
+
+extension CarViewController: WKNavigationDelegate, WKUIDelegate {
+       func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+       loading.stopAnimating()
+           webView.evaluateJavaScript("alert('É isso ai pe-p-p-pe-pessoal!!!')") { (returnObject, error) in
+               print(returnObject)
+           }
+    }
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
+        print(message)
+        completionHandler()
+    }
+}
+
